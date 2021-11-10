e66e6d9a31e16952512ffca6f057a8f65ff05d85
Kalvin Wettstein <kalvinwettstein@gmail.com>
2021-11-09 22:41:05 -0700

    search bar

--- wordsearch/urls.py
+++ wordsearch/urls.py
@@ -4,4 +4,5 @@ from .views import *
 
 urlpatterns = [
     path('', indexPageView, name='index'),
+    path('<str:search_word>/', resultsPageView, name='results')
 ]
--- wordsearch/views.py
+++ wordsearch/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 def indexPageView(req):
     return render(req, 'wordsearch/index.html')
+
+def resultsPageView(req, search_word):
+    context = {'search_word': search_word}
+    return render(req, 'wordsearch/results.html', context)
