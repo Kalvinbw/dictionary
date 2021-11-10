6292713379d95c05f5c51846216eb3eea58bfdc9
Kalvin Wettstein <kalvinwettstein@gmail.com>
2021-11-10 13:18:03 -0700

    hide secrets

--- dictionary/settings.py
+++ dictionary/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 from getpass import getpass
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o^3u#tv@dd=uvo8ih++)23giczmwuru9@t-amfd#)a9pr=z0me'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -81,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'dictionary',
         'USER': 'postgres',
-        'PASSWORD': 'dictionaryDB', #getpass(),
+        'PASSWORD': str(os.getenv('DB_PASSWORD')), #getpass(),
         'HOST': 'localhost'
     }
 }
