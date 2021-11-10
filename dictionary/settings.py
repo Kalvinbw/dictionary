c656a54273304cadf0e2d700553a180b4e1aa8ec
Kalvin Wettstein <kalvinwettstein@gmail.com>
2021-11-10 09:10:11 -0700

    add postgress connection

--- dictionary/settings.py
+++ dictionary/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+from getpass import getpass
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,8 +78,11 @@ WSGI_APPLICATION = 'dictionary.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dictionary',
+        'USER': 'postgres',
+        'PASSWORD': 'dictionaryDB', #getpass(),
+        'HOST': 'localhost'
     }
 }
 
--- wordsearch/models.py
+++ wordsearch/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class Search(models.Model):
+    search_word = models.CharField(max_length=50)
+    datetime = models.DateTimeField(default=datetime.now)
+
+    def __str__(self):
+        return (self.search_word)
+    
+    @property
+    def getSearchWord(self):
+        return (self.search_word)   
