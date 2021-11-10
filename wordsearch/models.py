edf5e2350f3369c1087084c937623d142a218ee7
Kalvin Wettstein <kalvinwettstein@gmail.com>
2021-11-10 13:24:11 -0700

    datetime_searched

--- wordsearch/models.py
+++ wordsearch/models.py
@@ -4,7 +4,7 @@ from datetime import datetime
 # Create your models here.
 class Search(models.Model):
     search_word = models.CharField(max_length=50)
-    datetime = models.DateTimeField(default=datetime.now)
+    datetime_searched = models.DateTimeField(default=datetime.now)
 
     def __str__(self):
         return (self.search_word)
